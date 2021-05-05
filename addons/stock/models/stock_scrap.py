828b04aa248f384a70748d5255f86e8d9dcd07e1
andrestvp <andrest.9324@gmail.com>
2021-05-05 19:56:39 +0000

    cambiosdardebaja

--- addons/stock/models/stock_scrap.py
+++ addons/stock/models/stock_scrap.py
@@ -28,7 +28,7 @@ class StockScrap(models.Model):
         copy=False, readonly=True, required=True,
         states={'done': [('readonly', True)]})
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, required=True, states={'done': [('readonly', True)]})
-    origin = fields.Char(string='Source Document')
+    origin = fields.Char(string='Source Document', default=' ', help="Motivo por cual esta dando de baja al producto")
     product_id = fields.Many2one(
         'product.product', 'Product', domain="[('type', 'in', ['product', 'consu']), '|', ('company_id', '=', False), ('company_id', '=', company_id)]",
         required=True, states={'done': [('readonly', True)]}, check_company=True)
