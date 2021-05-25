ca3dff3e92b153d143ea219b15322c14c9dd462c
andrestvp <andrest.9324@gmail.com>
2021-05-25 17:04:42 +0000

    Generacion Reporte

--- credito/models/models.py
+++ credito/models/models.py
@@ -16,13 +16,3 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
-
-from odoo import models, fields, api
-
-class  credito(models.MOdel):
-    _name = 'credito.credito'
-    _descripcion = 'credito.credito'
-
-    name = fields.Char(string, required = True)
-
-
