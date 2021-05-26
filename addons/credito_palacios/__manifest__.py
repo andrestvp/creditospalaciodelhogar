8c46fdce6fa97bfb10aa167f2ca88854cb0ab493
andrestvp <andrest.9324@gmail.com>
2021-05-26 16:15:39 +0000

    Modificacion Mannifest

--- addons/credito_palacios/__manifest__.py
+++ addons/credito_palacios/__manifest__.py
@@ -3,21 +3,20 @@
     'name': "credito_palacios",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulo orientado a la gestion de credito""",
 
     'description': """
-        Long description of module's purpose
+        Sistema de Gestion Crediticia 
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Credito Palacio del Hogar",
+    'website': "http://www.creditopalaciodelhogar.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Inventory',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -32,4 +31,7 @@
     'demo': [
         'demo/demo.xml',
     ],
+
+	'application':True
+	'auto_installable':True
 }
